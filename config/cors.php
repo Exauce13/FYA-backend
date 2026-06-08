cc5b5bb935f586e2060ca6c00840a61e111b8048
Exauce13 <jeanpaulgnikpo65@gmail.com>
2026-06-08 12:29:38 +0100

    modification

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['192.168.1.93:5173'],
+    'allowed_origins' => ['http://192.168.1.93:5173'],
 
     'allowed_origins_patterns' => [],
 
